94e2cfd439e64eb58f96ab2842c9c33d9a720a82
L1Pendz <2007rlee@gmail.com>
2026-08-20 09:09:36 +0800

    initial commit

---
+++ src/customerRoutes.ts
@@ -0,0 +1,52 @@
+import { Router, Request, Response } from "express";
+import { pool } from "./db";
+import { Customer } from "./types";
+
+const router = Router();
+
+router.get("/customers", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `SELECT customer_id, customer_name, city, membership_level
+      FROM customers`,
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+router.get("/customers/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `SELECT customer_id, customer_name, city, membership_level
+        FROM customers
+        WHERE customer_id = $1`,
+      [id],
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: "Customer not found" });
+      return;
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+router.post("/customer", async (req: Request, res: Response) => {
+  const { customer_id, customer_name, city, membership_level }: Customer =
+    req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO customer (customer_id, customer_name, city, membership_level)
+            VALUES ($1, $2, $3, $4)
+            RETURNING customer_id, customer_name, city, membership_level`,
+      [customer_id, customer_name, city, membership_level],
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
---
+++ src/types.ts
@@ -0,0 +1,39 @@
+export interface Customer {
+  customer_id: string;
+  customer_name: string;
+  city: string;
+  membership_level: string;
+}
+
+export interface Order {
+  order_id: string;
+  custormer_id: string;
+  order_date: Date;
+  shipping_city: string;
+}
+
+export interface Product {
+  product_id: string;
+  product_name: string;
+  category: string;
+  unit_price: number;
+}
+
+export interface OrderItem {
+  order_id: string;
+  product_id: string;
+  quantity: number;
+  discount: number;
+}
+
+export interface Vendor {
+  vendor_id: string;
+  vendor_name: string;
+  city: string;
+}
+
+export interface Supplies {
+  vendor_id: string;
+  product_id: number;
+  stock_quantity: string;
+}
