eeb3dd500767f2e6e8d0f33d0e4a006a9d50fcc3
christophercolomer <colomerchristopher@gmail.com>
2026-08-21 21:34:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import customerRoutes from "./customerRoutes";
 import productRoutes from "./productRoutes";
+import orderItemRoutes from "./orderItemRoutes";
+import orderRoutes from "./orderRoutes";
+import vendorSuppliesRoutes from "./vendorSuppliesRoutes"
 
 dotenv.config();
 
@@ -12,6 +15,9 @@ app.use(express.json());
 
 app.use("/api", customerRoutes);
 app.use("/api", productRoutes);
+app.use("/api", orderItemRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", vendorSuppliesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Ecommerce API server running on http://localhost:${PORT}`);
